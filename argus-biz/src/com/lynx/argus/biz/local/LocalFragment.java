57fc5d7d3e18d2ce56dcbbdcb88b098ca2944de0
zhufeng.liu <zhufeng.liu@dianping.com>
2013-09-13 13:08:57 +0800

    添加下拉刷新组件

--- argus-biz/src/com/lynx/argus/biz/local/LocalFragment.java
+++ argus-biz/src/com/lynx/argus/biz/local/LocalFragment.java
@@ -10,16 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.lynx.argus.R;
 import com.lynx.argus.app.BasicFragment;
 import com.lynx.argus.app.BizApplication;
 import com.lynx.argus.biz.SysInfoActivity;
+import com.lynx.lib.http.core.AsyncTask;
 import com.lynx.service.geo.GeoService;
 import com.lynx.service.geo.LocationListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,11 +34,15 @@ import com.lynx.service.geo.LocationListener;
  */
 public class LocalFragment extends BasicFragment {
     private GeoService geoService;
+    private List<Map<String, Object>> idxInfos = new ArrayList<Map<String, Object>>();
 
     private Animation animRotate;
     private AnimationDrawable adIndicator;
     private TextView tvLocAddr;
     private ImageView ivLocIndicator, ivLocRefresh;
+    private PullToRefreshListView prlvIdx;
+
+    private static final String BMAP_PLACE = "http://api.map.baidu.com/place/v2/search?&query=%s&location=%s,%s&radius=2000&output=json&ak=%s";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,19 +54,29 @@ public class LocalFragment extends BasicFragment {
         animRotate.setRepeatCount(-1);
         animRotate.setRepeatMode(Animation.RESTART);
 
+        prlvIdx = (PullToRefreshListView) view.findViewById(R.id.prlv_local_idx);
         ivLocIndicator = (ImageView) view.findViewById(R.id.iv_main_loc_indicator);
         tvLocAddr = (TextView) view.findViewById(R.id.tv_main_loc_addr);
         ivLocRefresh = (ImageView) view.findViewById(R.id.iv_main_loc_refresh);
         ivLocRefresh.setAnimation(animRotate);
         animRotate.cancel();
 
-        Button btn = (Button) view.findViewById(R.id.btn_main_load_apk_a);
-        btn.setOnClickListener(new View.OnClickListener() {
+        initIdx();
+        SimpleAdapter adapter = new SimpleAdapter(this.getActivity(), idxInfos,
+                R.layout.layout_local_idx_item,
+                new String[]{"icon", "title", "desc"},
+                new int[]{R.id.iv_local_idx_item_icon, R.id.tv_local_idx_item_title, R.id.tv_local_idx_item_desc}
+        );
+        prlvIdx.setAdapter(adapter);
+        prlvIdx.setOnRefreshListener(new PullToRefreshListView.OnRefreshListener() {
             @Override
-            public void onClick(View view) {
-                tabActivity.pushFragments("Local", new LocalMapFragment(), true, true);
+            public void onRefresh() {
+                // Do work to refresh the list here.
+                new GetDataTask().execute();
             }
         });
+//      tabActivity.pushFragments("Local", new LocalMapFragment(), true, true);
+
 
         ImageView ivIndicator = (ImageView) view.findViewById(R.id.iv_main_loc_indicator);
         ivIndicator.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +93,69 @@ public class LocalFragment extends BasicFragment {
 
 
     }
+
+    private void initIdx() {
+        Map<String, Object> idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.food_u + "");
+        idxInfo.put("title", "小吃快餐");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.coffee_u + "");
+        idxInfo.put("title", "咖啡屋");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.bread_u + "");
+        idxInfo.put("title", "西点");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.pot_u + "");
+        idxInfo.put("title", "火锅");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.ktv_u + "");
+        idxInfo.put("title", "KTV");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.film_u + "");
+        idxInfo.put("title", "电影院");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.hotel_u + "");
+        idxInfo.put("title", "酒店");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.bank_u + "");
+        idxInfo.put("title", "银行");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.hair_u + "");
+        idxInfo.put("title", "美容美发");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+
+        idxInfo = new HashMap<String, Object>();
+        idxInfo.put("icon", R.drawable.park_u + "");
+        idxInfo.put("title", "停车场");
+        idxInfo.put("desc", "asdfasd.fadsf");
+        idxInfos.add(idxInfo);
+    }
+
 
     private Handler handler = new Handler() {
         @Override
@@ -96,6 +179,27 @@ public class LocalFragment extends BasicFragment {
             }
         }
     };
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                ;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            // Call onRefreshComplete when the list has been refreshed.
+            prlvIdx.onRefreshComplete();
+            super.onPostExecute(result);
+        }
+    }
+
 
     /**
      * 初始化定位相关模块
